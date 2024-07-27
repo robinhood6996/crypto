bbeb1bf111e4d2ff511cf0e805576dfde7a012be
Fakrul Islam Robin <fakrulrobin7@gmail.com>
2024-07-27 19:29:16 +0200

    update layout

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -1,56 +1,58 @@
-import {Outlet} from "react-router-dom";
-import {IoHome} from "react-icons/io5";
-import {IoMdCart} from "react-icons/io";
-import {FaCircleUser, FaShop} from "react-icons/fa6";
-import {FaWallet} from "react-icons/fa";
-
+import { Link, Outlet } from "react-router-dom";
+import { IoHome } from "react-icons/io5";
+import { IoMdCart } from "react-icons/io";
+import { FaCircleUser, FaShop } from "react-icons/fa6";
+import { FaWallet } from "react-icons/fa";
+import Logo from "../assets/images/logo.png";
 function Layout() {
-    return (
-        <>
-        <header className="header" id="header">
-            <nav className="navbar container">
-                <a href="./index.html" className="brand">Name</a>
-                <div className="menu" id="menu">
-                    <ul className="menu-list">
-                        <li className="menu-item">
-                            <a href="#" className="menu-link is-active">
-                                <IoHome className="menu-icon"/>
-                                <span className="menu-name">Home</span>
-                            </a>
-                        </li>
-                        <li className="menu-item">
-                            <a href="#" className="menu-link">
-                                <IoMdCart className="menu-icon" />
-                                <span className="menu-name">Order</span>
-                            </a>
-                        </li>
-                        <li className="menu-item">
-                            <a href="#" className="menu-link">
-                                <FaShop className="menu-icon" />
-                                <span className="menu-name">Market</span>
-                            </a>
-                        </li>
-                        <li className="menu-item">
-                            <a href="#" className="menu-link">
-                                <FaWallet className="menu-icon" />
-                                <span className="menu-name">Asset</span>
-                            </a>
-                        </li>
-                        <li className="menu-item">
-                            <a href="#" className="menu-link">
-                                <FaCircleUser className="menu-icon" />
-                                <span className="menu-name">Account</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-        <div className="position-relative main-wrapper">
-            <Outlet />
-        </div>
+  return (
+    <>
+      <header className="header" id="header">
+        <nav className="navbar container">
+          <Link to="/">
+            <img src={Logo} alt="logo" height={60} width={80} />
+          </Link>
+          <div className="menu" id="menu">
+            <ul className="menu-list">
+              <li className="menu-item">
+                <Link to="#" className="menu-link is-active">
+                  <IoHome className="menu-icon" />
+                  <span className="menu-name">Home</span>
+                </Link>
+              </li>
+              <li className="menu-item">
+                <Link to="/orders" className="menu-link">
+                  <IoMdCart className="menu-icon" />
+                  <span className="menu-name">Order</span>
+                </Link>
+              </li>
+              <li className="menu-item">
+                <Link to="/market" className="menu-link">
+                  <FaShop className="menu-icon" />
+                  <span className="menu-name">Market</span>
+                </Link>
+              </li>
+              <li className="menu-item">
+                <Link to="/assets" className="menu-link">
+                  <FaWallet className="menu-icon" />
+                  <span className="menu-name">Asset</span>
+                </Link>
+              </li>
+              <li className="menu-item">
+                <Link to="/account" className="menu-link">
+                  <FaCircleUser className="menu-icon" />
+                  <span className="menu-name">Account</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <div className="position-relative main-wrapper">
+        <Outlet />
+      </div>
     </>
-    );
+  );
 }
 
 export default Layout;
